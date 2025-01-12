6504811e76da36b8d3b2ef999334d9154d5d6d63
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-01-12 15:48:53 -0800

    build api

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,7 +8,7 @@ import 'easymde/dist/easymde.min.css';
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/validationSchema';
+import { IssueSchema } from '@/app/validationSchema';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
@@ -19,7 +19,7 @@ const SimpleMDE = dynamic(
   { ssr: false }
 );
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof IssueSchema>;
 
 interface Props {
   issue?: Issue
@@ -28,7 +28,7 @@ interface Props {
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
   const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema)
+    resolver: zodResolver(IssueSchema)
   });
   const [error, setError] = useState('');
   const [isSubmitting, setSubmitting] = useState(false);
