c4674db7432c6da31db469ca06beecc18b480203
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-02-22 12:41:14 -0800

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,7 +36,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data);
+      if(issue)
+        await axios.patch('/api/issues/' + issue.id, data);
+      else
+        await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
       setSubmitting(false);
@@ -68,7 +71,9 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
             {errors.description?.message}
           </ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue{isSubmitting && <Spinner />}</Button>
+          {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+          {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   )
